2c0106fb17eebf5fd0041a12eb03de5edb1794cf
Alexandre FAYE <alexandre.faye-bedrin@ens-rennes.fr>
2018-06-13 11:55:57 +0200

    ajout de la notif par SMS parce que c'est drôle

--- make_run.bat
+++ make_run.bat
@@ -1,2 +1,4 @@
 gcc noeud.c -o noeud.exe -lm -lpthread -std=gnu99 -Ofast -march=native
 noeud.exe
+rem notif par SMS (manque ID&pass)
+rem certutil -urlcache -split -f "https://smsapi.free-mobile.fr/sendmsg?user= &pass= &msg=Calcul%20terminé%20!" sms.html
